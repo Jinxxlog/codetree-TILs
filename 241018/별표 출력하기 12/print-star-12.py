2e4e3e4952bca2b627272de311b6a0d335f9c3c6
Jinhyung Kim <mir001125@naver.com>
2024-10-18 17:04:52 +0900

    Title: 별표 출력하기 12, Time: 214ms, Memory: 25MB, Status: Passed - CodeTree

--- 241018/별표 출력하기 12/print-star-12.py
+++ 241018/별표 출력하기 12/print-star-12.py
@@ -1,12 +1,4 @@
 n = int(input())
-
-print('* ' * n)
-    
+print('* '*n)
 for i in range(1, n):
-    line = ''
-    for j in range(n):
-        if j == 1 or (j > 1 and j % 3 == 1 and i < j):
-            line += '* '
-        else:
-            line += '  '
-    print(line.rstrip())
+    print('    '*(i//2), '  * '*(n//2-i//2), sep='')
